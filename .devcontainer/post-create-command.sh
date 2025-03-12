86b1dbae790b745467743d623d184f9e502253dc
aaglietti-itsrizzoli <104371332+aaglietti-itsrizzoli@users.noreply.github.com>
2025-03-12 23:14:22 +0000

    feat: devcontainer

--- .devcontainer/post-create-command.sh
+++ .devcontainer/post-create-command.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "post-create-command.sh"
+
 sudo cp tor.list /etc/apt/sources.list.d/
 
 wget -qO- https://deb.torproject.org/torproject.org/A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89.asc | gpg --dearmor | sudo tee /usr/share/keyrings/deb.torproject.org-keyring.gpg >/dev/null
