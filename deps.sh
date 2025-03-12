71fe0ec62b1bf31ccb1d71d802f2ab7679bddad9
aaglietti-itsrizzoli <104371332+aaglietti-itsrizzoli@users.noreply.github.com>
2025-03-12 22:58:50 +0000

    fix: deps

--- deps.sh
+++ deps.sh
@@ -5,6 +5,6 @@ sudo cp tor.list /etc/apt/sources.list.d/
 wget -qO- https://deb.torproject.org/torproject.org/A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89.asc | gpg --dearmor | sudo tee /usr/share/keyrings/deb.torproject.org-keyring.gpg >/dev/null
 
 sudo apt update
-sudo apt install -y tor deb.torproject.org-keyring
+sudo apt install -y tor deb.torproject.org-keyring apt-transport-https
 
 sudo /etc/init.d/tor restart
