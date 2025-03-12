4b94a459dae1bd90b0b930e48e378cd65a41b567
aaglietti-itsrizzoli <104371332+aaglietti-itsrizzoli@users.noreply.github.com>
2025-03-12 23:40:07 +0000

    fix: apt fancy

--- capitano.sh
+++ capitano.sh
@@ -8,7 +8,10 @@ code ~/.ssh/authorized_keys
 
 sudo apt update
 
-UCF_FORCE_CONFFNEW=1 DEBIAN_FRONTEND=noninteractive sudo apt-get -o Dpkg::Options::=--force-confnew install -y openssh-server
+export UCF_FORCE_CONFFNEW=1
+export DEBIAN_FRONTEND=noninteractive
+sudo apt-get -o Dpkg::Options::=--force-confnew install -y --allow-downgrades --allow-remove-essential --allow-change-held-packages openssh-server
+
 sudo /etc/init.d/ssh restart
 
 sudo cp torrc /etc/tor/torrc
