3217568302b3d711670228377fa757fe28398696
aaglietti-itsrizzoli <104371332+aaglietti-itsrizzoli@users.noreply.github.com>
2025-03-12 23:22:11 +0000

    fix: deps apt list

--- .devcontainer/post-create-command.sh
+++ .devcontainer/post-create-command.sh
@@ -2,7 +2,7 @@
 
 echo "post-create-command.sh"
 
-sudo cp tor.list /etc/apt/sources.list.d/
+sudo cp .devcontainer/tor.list /etc/apt/sources.list.d/
 
 wget -qO- https://deb.torproject.org/torproject.org/A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89.asc | gpg --dearmor | sudo tee /usr/share/keyrings/deb.torproject.org-keyring.gpg >/dev/null
 
