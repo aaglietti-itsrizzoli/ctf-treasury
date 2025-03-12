f632d9c5790e8b2c1d2b5844d55d74accc0167d5
aaglietti-itsrizzoli <104371332+aaglietti-itsrizzoli@users.noreply.github.com>
2025-03-12 22:31:06 +0000

    feat: deps

---
+++ deps.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo cp tor.list /etc/apt/sources.list.d/
+
+wget -qO- https://deb.torproject.org/torproject.org/A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89.asc | gpg --dearmor | sudo tee /usr/share/keyrings/deb.torproject.org-keyring.gpg >/dev/null
+
+sudo apt update
+sudo apt install tor deb.torproject.org-keyring
+
+sudo /etc/init.d/tor restart
