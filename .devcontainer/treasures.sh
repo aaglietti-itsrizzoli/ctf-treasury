ad6fec10571ae1814e01cddb6d374124f2a32fcf
aaglietti-itsrizzoli <104371332+aaglietti-itsrizzoli@users.noreply.github.com>
2025-03-13 00:16:47 +0000

    fix: codespace as proof of treassures

--- .devcontainer/treasures.sh
+++ .devcontainer/treasures.sh
@@ -8,33 +8,33 @@ sudo chmod 777 -R /start
 ruby .devcontainer/maze.rb ${HOW_MANY} /start
 
 dst=`find /start -name 29`
-echo "Tesoro #1" > treasure.txt
+echo "Tesoro #1 @ $CODESPACE_NAME" > treasure.txt
 echo $dst >> treasure.txt
-printf $dst | sha1sum - | awk '{print $1}' >> treasure.txt
+printf ${dst}${CODESPACE_NAME} | sha1sum - | awk '{print $1}' >> treasure.txt
 cp treasure.txt ${dst}
 
 dst=`find /start -name 09`
-echo "Tesoro #2" > treasure.txt
+echo "Tesoro #2 @ $CODESPACE_NAME" > treasure.txt
 echo $dst >> treasure.txt
-printf $dst | sha1sum - | awk '{print $1}' >> treasure.txt
+printf ${dst}${CODESPACE_NAME} | sha1sum - | awk '{print $1}' >> treasure.txt
 cp treasure.txt ${dst}
 
 dst=`find /start -name 02`
-echo "Tesoro #3" > treasure.txt
+echo "Tesoro #3 @ $CODESPACE_NAME" > treasure.txt
 echo $dst >> treasure.txt
-printf $dst | sha1sum - | awk '{print $1}' >> treasure.txt
+printf ${dst}${CODESPACE_NAME} | sha1sum - | awk '{print $1}' >> treasure.txt
 cp treasure.txt ${dst}
 
 dst=`find /start -name 07`
-echo "Tesoro #4" > treasure.txt
+echo "Tesoro #4 @ $CODESPACE_NAME" > treasure.txt
 echo $dst >> treasure.txt
-printf $dst | sha1sum - | awk '{print $1}' >> treasure.txt
+printf ${dst}${CODESPACE_NAME} | sha1sum - | awk '{print $1}' >> treasure.txt
 cp treasure.txt ${dst}
 
 dst=`find /start -name 10`
-echo "Tesoro #5" > treasure.txt
+echo "Tesoro #5 @ $CODESPACE_NAME" > treasure.txt
 echo $dst >> treasure.txt
-printf $dst | sha1sum - | awk '{print $1}' >> treasure.txt
+printf ${dst}${CODESPACE_NAME} | sha1sum - | awk '{print $1}' >> treasure.txt
 cp treasure.txt ${dst}
 
 echo 'Questo non è uno dei tesori che vai cercando!!!' > treasure.txt
