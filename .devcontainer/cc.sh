e793ceb4b006c5553c7413b11063aa4ba5adb253
aaglietti-itsrizzoli <104371332+aaglietti-itsrizzoli@users.noreply.github.com>
2025-03-14 07:55:57 +0000

    fix: qc dashboard as main http page

--- .devcontainer/cc.sh
+++ .devcontainer/cc.sh
@@ -11,7 +11,7 @@ sudo cp .devcontainer/.htaccess /var/www/html/cc/.htaccess
 sudo cp .devcontainer/index.py /var/www/html/cc/index.py
 sudo chmod +x /var/www/html/cc/index.py
 
-sudo cp .devcontainer/cc.html /var/www/html/cc.html
+sudo cp .devcontainer/cc.html /var/www/html/index.html
 sudo cp .devcontainer/jquery-3.7.1.min.js /var/www/html/jquery-3.7.1.min.js
 
 sudo cp .devcontainer/000-default.conf /etc/apache2/sites-available/000-default.conf
